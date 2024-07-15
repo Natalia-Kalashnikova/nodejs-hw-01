df0fc6c7357d1a3755cfe3f7118021f22d172cef
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-07-16 00:28:32 +0300

    add countContacts function

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+    try {
+        let contactsFromDB = [];
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        contactsFromDB = JSON.parse(data);
+        return contactsFromDB.length;
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 console.log(await countContacts());
+
