a2259691b5d704e865ccda5c36fef3a53d239272
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-07-15 21:45:44 +0300

    add generateContacts function

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,26 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+
+const generateContacts = async (number) => {
+    try {
+        let contactsFromDB = [];
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        contactsFromDB = JSON.parse(data);
+
+        const newContacts = [];
+        for (let i = 0; i < number; i += 1){
+            newContacts.push(createFakeContact());
+        }
+
+        const updateContacts = [...contactsFromDB, ...newContacts];
+
+        await fs.writeFile(PATH_DB, JSON.stringify(updateContacts, undefined, 2));
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 generateContacts(5);
+
