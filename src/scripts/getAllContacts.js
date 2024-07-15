e73253fab32601fe69e8b7dd066653713c9ba066
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-07-16 00:08:19 +0300

    add getAllContacts function

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const getAllContacts = async () => {};
+
+export const getAllContacts = async () => {
+  try {
+    const contactsFromDB = await fs.readFile(PATH_DB, 'utf-8');
+    return JSON.parse(contactsFromDB);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 console.log(await getAllContacts());
