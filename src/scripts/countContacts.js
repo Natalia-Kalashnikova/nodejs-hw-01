9d974b6dd61b66ac7f9de0b50c8117e8d2c0e465
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-07-16 02:01:21 +0300

    add removeLastContact function

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,11 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
 import fs from 'fs/promises';
 
+
 export const countContacts = async () => {
     try {
         let contactsFromDB = [];
         const data = await fs.readFile(PATH_DB, 'utf-8');
-        contactsFromDB = JSON.parse(data);
+        contactsFromDB = JSON.parse(data, undefined, 2);
         return contactsFromDB.length;
     } catch (error) {
         console.log(error);
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,20 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeLastContact = async () => {};
+
+export const removeLastContact = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        const contactsFromDB = JSON.parse(data);
+
+        if (contactsFromDB.length > 0) {
+            contactsFromDB.pop();
+            console.log(contactsFromDB);
+            await fs.writeFile(PATH_DB, JSON.stringify(contactsFromDB));
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 removeLastContact();
