115bebff5d2f652eac1f5b7d863f38e2baa5be2c
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-07-15 22:02:27 +0300

    add addOneContact function

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,21 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+
+export const addOneContact = async () => {
+    try {
+        let contactsFromDB = [];
+        const data = await fs.readFile(PATH_DB, 'utf-8');
+        contactsFromDB = JSON.parse(data);
+
+        const oneContact = createFakeContact();
+        contactsFromDB.push(oneContact);
+
+        await fs.writeFile(PATH_DB, JSON.stringify(contactsFromDB, undefined, 2));
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 addOneContact();
